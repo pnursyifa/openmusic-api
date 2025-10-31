e49bd6b26010ecca2d4d79b267197f1ec0c85536
pnursyifa <pnursyifa@gmail.com>
2025-10-31 12:22:05 +0700

    feat: add users and auths

---
+++ migrations/1761883590681_create-table-users.js
@@ -0,0 +1,25 @@
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+    },
+  });
+};
+
+export const down = (pgm) => {
+  pgm.dropTable('users');
+};
